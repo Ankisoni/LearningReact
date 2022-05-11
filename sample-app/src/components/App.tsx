678229426d3d16438d78d7fe48b62e862e8f740c
NAGARRO\ankitagupta <ankita.gupat@nagarro.com>
2022-05-11 10:41:41 +0530

    Rendering component with Array

--- sample-app/src/components/App.tsx
+++ sample-app/src/components/App.tsx
@@ -1,13 +1,45 @@
 import React from 'react';
 import Card from './Card';
+import CardList from './CardList';
 
 
 function App() {
+
+  const cards =[
+    {
+      header:"Header 1",
+      title: "Title 1",
+      text:"With supporting text below as a natural lead-in to additional content.",
+      image:"https://picsum.photos/200/100"
+    },
+    {
+      header:"Header 2",
+      title: "Title 2",
+      text:"With supporting text below as a natural lead-in to additional content.",
+      image:"https://picsum.photos/200/100"
+    },
+    {
+      header:"Header 3",
+      title: "Title 3",
+      text:"With supporting text below as a natural lead-in to additional content.",
+      image:"https://picsum.photos/200/100"
+    },
+    {
+      header:"Header 4",
+      title: "Title 4",
+      text:"With supporting text below as a natural lead-in to additional content.",
+      image:"https://picsum.photos/200/100"
+    }
+  ]
+  
+
+
+
   return (
     <div className="App">
       <div className='container'>
         <br></br>
-      <Card></Card>
+      <CardList cards={cards}/>
       </div>
     </div>
   );
--- sample-app/src/components/CardList.tsx
+++ sample-app/src/components/CardList.tsx
@@ -1,1 +1,37 @@
 import React from 'react';
+
+const CardList = (props: any) => {
+    console.log(props);
+    const renderCardList = props.cards.map((card:any) => {
+        return (
+            <>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="card text-center ">
+                                <div className="card-header">
+                                    {card.header}
+                                </div>
+                                <div className="card-body">
+                                    <img className="card-img-top" src={card.image} alt="Card image" />
+
+                                    <h5 className="card-title">{card.title}</h5>
+                                    <p className="card-text">{card.text}</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    })
+    return (
+        <>
+        {renderCardList}
+        </>
+    );
+}
+
+
+export default CardList;
