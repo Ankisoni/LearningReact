412c65239edd8a23b55cc209e9267921ab9a3856
NAGARRO\ankitagupta <ankita.gupat@nagarro.com>
2022-05-11 23:03:06 +0530

    Modification to fetch data from json file

--- sample-app/src/components/App.tsx
+++ sample-app/src/components/App.tsx
@@ -2,44 +2,15 @@ import React from 'react';
 import Card from './Card';
 import CardList from './CardList';
 
-
 function App() {
-
-  const cards =[
-    {
-      header:"Header 1",
-      title: "Title 1",
-      text:"With supporting text below as a natural lead-in to additional content.",
-      image:"https://picsum.photos/200/100"
-    },
-    {
-      header:"Header 2",
-      title: "Title 2",
-      text:"With supporting text below as a natural lead-in to additional content.",
-      image:"https://picsum.photos/200/100"
-    },
-    {
-      header:"Header 3",
-      title: "Title 3",
-      text:"With supporting text below as a natural lead-in to additional content.",
-      image:"https://picsum.photos/200/100"
-    },
-    {
-      header:"Header 4",
-      title: "Title 4",
-      text:"With supporting text below as a natural lead-in to additional content.",
-      image:"https://picsum.photos/200/100"
-    }
-  ]
-  
-
-
-
+  const cards = require('../constants/cardData.json');
   return (
     <div className="App">
       <div className='container'>
         <br></br>
+        <div className='row'>
       <CardList cards={cards}/>
+      </div>
       </div>
     </div>
   );
--- sample-app/src/components/Card.tsx
+++ sample-app/src/components/Card.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 
-const Card = () =>{
+const Card = (props: any) => {
+    const { header, image, title, text } = props.card;
     return (
         <>
-        <div className="container">
-            <div className="row">
-                <div className="col-md-4">
-                    <div className="card text-center ">
-                        <div className="card-header">
-                            Card Header
-                        </div>
-                        <div className="card-body">
-                            <img className="card-img-top" src="https://picsum.photos/200/100" alt="Card image" />
+            <div className="container">
+                <div className="card text-center ">
+                    <div className="card-header">
+                        {header}
+                    </div>
+                    <div className="card-body">
+                        <img className="card-img-top" src={image} alt="Card image" />
 
-                            <h5 className="card-title">Card Title</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{text}</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
             </div>
-        </div>
+            <br></br>
         </>
     );
 
